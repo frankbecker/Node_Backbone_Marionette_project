a17ed9408a9babb588bfd2d283542733c1d5930b
frankbecker <Frank Becker>
2014-10-07 13:42:11 -0400

    modified a few things

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // config/database.js
 module.exports = {
-	'url' : 'mongodb://localhost/social_network' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://localhost/social_network' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url'  :  'mongodb://heroku_app30169851:hb2e33450fia67t7pp5nb6jc73@ds041150.mongolab.com:41150/heroku_app30169851'
 };
